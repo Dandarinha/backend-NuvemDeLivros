9e59690685e18191ab791fb5cec5c0c22aae3174
Dandara Gabrielli <dandaragabrielle00@gmail.com>
2024-11-05 13:54:56 -0300

    cadastrando e listando

--- src/model/Livro.ts
+++ src/model/Livro.ts
@@ -1,3 +1,7 @@
+import { DatabaseModel } from "./DatabaseModel";
+
+const database = new DatabaseModel().pool;
+
 /*
  Classe que representa um Livro.
  */
@@ -104,4 +108,102 @@ export class Livro {
     public setStatusLivroEmprestado(statusLivroEmprestado: string): void {
       this.statusLivroEmprestado = statusLivroEmprestado;
     }
+
+  /**
+     * Busca e retorna uma lista de Livros do banco de dados.
+     * @returns Um array de objetos do tipo `Livro` em caso de sucesso ou `null` se ocorrer um erro durante a consulta.
+     * 
+     * - A função realiza uma consulta SQL para obter todas as informações da tabela "Livro".
+     * - Os dados retornados do banco de dados são usados para instanciar objetos da classe `Livro`.
+     * - Cada Livro é adicionado a uma lista que será retornada ao final da execução.
+     * - Se houver falha na consulta ao banco, a função captura o erro, exibe uma mensagem no console e retorna `null`.
+     */
+
+  static async listagemLivro(): Promise<Array<Livro> | null> {
+    // objeto para armazenar a lista de Livros
+    const listaDeLivro: Array<Livro> = [];
+
+    try {
+        // query de consulta ao banco de dados
+        const querySelectLivro = `SELECT * FROM livro;`;
+
+        // fazendo a consulta e guardando a resposta
+        const respostaBD = await database.query(querySelectLivro);
+
+        // usando a resposta para instanciar um objeto do tipo Livro
+        respostaBD.rows.forEach((linha) => {
+            // instancia (cria) objeto Livro
+            const novoLivro = new Livro(linha.titulo,
+                                        linha.autor,
+                                        linha.editora,
+                                        linha.anoPublicacao,
+                                        linha.isbn,
+                                        linha.quantTotal,
+                                        linha.quantDisponivel,
+                                        linha.valorAquisicao,
+                                        linha.statusLivroEmprestado);
+
+            // atribui o ID objeto
+            novoLivro.setIdLivro(linha.id_Livro);
+
+            // adiciona o objeto na lista
+            listaDeLivro.push(novoLivro);
+        });
+
+        // retorna a lista de Livros
+        return listaDeLivro;
+    } catch (error) {
+        console.log('Erro ao buscar lista de Livro');
+        return null;
+    }
+
+/**
+ * Realiza o cadastro de um Livro no banco de dados.
+ * @param {Livro} Livro 
+ * @returns {Promise<boolean>} 
+ * 
+ * @throws {Error} - Se ocorrer algum erro durante a execução do cadastro, uma mensagem de erro é exibida
+ *                   no console junto com os detalhes do erro.
+ */
   }
+
+static async cadastroLivro(Livro: Livro): Promise<any> {
+    try {
+        // query para fazer insert de um Livro no banco de dados
+        const queryInsertLivro = `INSERT INTO Livro (nome, sobrenome, datadenascimento, endereco, email, celular)
+                                    VALUES
+                                    ('${Livro.getIdLivro()}', 
+                                    '${Livro.getTitulo()}', 
+                                    '${Livro.getAutor()}', 
+                                    '${Livro.getEditora()}',
+                                    '${Livro.getAnoPublicacao()}',
+                                    '${Livro.getIsbn()},
+                                    '${Livro.getQuantTotal()},
+                                    '${Livro.getValorAquisicao()},
+                                    '${Livro.getStatusLivroEmprestado()},
+                                    ')
+                                    RETURNING id_Livro;`;
+
+        // executa a query no banco e armazena a resposta
+        const respostaBD = await database.query(queryInsertLivro);
+
+        // verifica se a quantidade de linhas modificadas é diferente de 0
+        if (respostaBD.rowCount != 0) {
+            console.log(`Livro cadastrado com sucesso! ID do Livro: ${respostaBD.rows[0].id_Livro}`);
+            // true significa que o cadastro foi feito
+            return true;
+        }
+        // false significa que o cadastro NÃO foi feito.
+        return false;
+
+        // tratando o erro
+    } catch (error) {
+        // imprime outra mensagem junto com o erro
+        console.log('Erro ao cadastrar o Livro. Verifique os logs para mais detalhes.');
+        // imprime o erro no console
+        console.log(error);
+        // retorno um valor falso
+        return false;
+    }
+}
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,37 @@
 import { Request, Response, Router } from "express";
+import { AlunoController} from "./controller/AlunoController";
+import { EmprestimoController } from "./controller/EmprestimoController";
+import { LivroController } from "./controller/LivroController";
 
 // Cria um roteador
 const router = Router();
 
-//Criando uma rota principal para a aplicação
+// Criando uma rota principal para a aplicação
 router.get("/", (req: Request, res: Response) => {
-    res.json({ mensagem: "Olá, mundo!"});
+    res.json({ mensagem: "Olá, mundo!" });
 });
 
-//exportando as rotas
-export {router};
+/* 
+* ROTAS PARA AlunoS
+*/ 
+// Rota para listar os Alunos
+router.get("/lista/Alunos", AlunoController.todos);
+// Rota para cadastrar um novo Aluno
+router.post("/novo/Aluno", AlunoController.novo);
+
+/* 
+* ROTAS PARA LIVROS
+*/ 
+// Rota para listar os Livro
+router.get("/lista/livros", LivroController.todos);
+// Rota para cadastrar um novo pedido
+router.post("/novo/livro", LivroController.novo);
+
+/* 
+* ROTAS PARA EMPRESTIMOS
+*/ 
+// Rota para listar os EmprestimoControllers
+router.get("/lista/emprestimo", EmprestimoController.todos);
+
+// exportando as rotas
+export { router };
